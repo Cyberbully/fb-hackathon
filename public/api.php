d3bb45cc0aa0eacd5975491e86b19d4cd589e2ec
Oliver Tan <sinovercosequalstan@gmail.com>
2015-04-18 16:12:54 +1000

    hey g00rge

--- public/api.php
+++ public/api.php
@@ -9,38 +9,47 @@ use Parse\ParseQuery;
 $app = new \Slim\Slim();
 
 $app->get('/api/hello/:name', function ($name) {
-    echo "Hello, $name, my name is Wayne Wobcke";
+    echo 'Hello, $name, my name is Wayne Wobcke';
 });
 
 $app->post('/api/create', function() use ($app) {
     $data = json_decode($app->request()->getBody(), true);
-    
-    $eventTable = new ParseObject("Event");
-    $eventTable->set("event_id", $data["event_id"]);
-    $eventTable->set("start_time", $data["start_time"]);
-    $eventTable->set("end_time", $data["end_time"]);
-    $eventTable->set("frequency", $data["frequency"]);
-    $eventTable->setAssociativeArray("times", []);
-    $eventTable->setAssociativeArray("entries", []);
+     
+    $event = new ParseObject('Event');
+    $event->set('event_id', $data['event_id']);
+    $event->set('start_time', $data['start_time']);
+    $event->set('end_time', $data['end_time']);
+    $event->set('frequency', $data['frequency']);
+    $event->setAssociativeArray('times', []);
+    $event->setAssociativeArray('entries', []);
 
     try {
-        $eventTable->save();
+        $event->save();
     } catch (ParseException $ex) {
         echo json_encode(array('ok' => false, 'error' => $ex->getMessage()));
         return;
     }
 
-    echo json_encode(array('ok' => true, 'event_id'=>$data["event_id"]));
+    echo json_encode(array('ok' => true, 'event_id'=>$data['event_id']));
 });
 
-$app->get('/api/event/:event_id', function(event_id) {
-    $query = new ParseQuery("Event");
+$app->get('/api/event/:event_id', function($event_id) {
+    $query = new ParseQuery('Event');
     try {
-        
+        $event = $query->get($event_id);
     } catch (ParseException $ex) {
         echo json_encode(array('ok' => false, 'error' => $ex->getMessage()));
         return;
     }
+
+    echo json_encode(array('ok' => true, 'event' => array(
+        'event_id' => $event->get('event_id'),
+        'start_time' => $event->get('start_time'),
+        'end_time' => $event->get('end_time'),
+        'frequency' => $event->get('frequency'),
+        'times' => $event->get('times'),
+        'entries' => $event->get('entries'),
+    ))); 
 });
 
 $app->run();
