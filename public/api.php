280cbd6ee5841fac988791de791d0a9adfefc358
George Caley <george@caley.com.au>
2015-04-18 16:39:59 +1000

    added test post endpoint

--- public/api.php
+++ public/api.php
@@ -4,6 +4,9 @@ require_once '../app/config.php';
 
 use Parse\ParseClient;
 use Parse\ParseObject;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
 
 $app = new \Slim\Slim();
 
@@ -32,6 +35,30 @@ $app->post('/api/create', function() use ($app) {
 
     $reply = array('ok' => true, 'event_id'=>$data["event_id"]);
     echo json_encode($reply);
+});
+
+$app->post('/api/create2', function() use ($app) {
+    $data = json_decode($app->request()->getBody(), true);
+
+    $eventID = $data['event_id'];
+
+    session_start();
+    $session = getSession();
+    if (!$session) {
+        echo "no session 4 u";
+        return;
+    }
+
+    $request = new FacebookRequest(
+        $session,
+        'POST',
+        '/' . $eventID . '/feed',
+        array (
+            'message' => 'here is a test message',
+        )
+    );
+    $response = $request->execute();
+    $graphObject = $response->getGraphObject();
 });
 
 $app->run();
