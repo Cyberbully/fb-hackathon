cdaeec893dc7d8c5bd0e2fa5bec67a7a890259e7
Oliver Tan <sinovercosequalstan@gmail.com>
2015-04-18 15:56:51 +1000

    create endpoint

--- public/api.php
+++ public/api.php
@@ -2,10 +2,36 @@
 require_once __DIR__ . '/../app/vendor/autoload.php';
 require_once '../app/config.php';
 
+use Parse\ParseClient;
+use Parse\ParseObject;
+
 $app = new \Slim\Slim();
 
-$app->get('/hello/:name', function ($name) {
+$app->get('/api/hello/:name', function ($name) {
     echo "Hello, $name, my name is Wayne Wobcke";
+});
+
+$app->post('/api/create', function() use ($app) {
+    $data = json_decode($app->request()->getBody(), true);
+    
+    $eventTable = new ParseObject("EventTable");
+    $eventTable->set("event_id", $data["event_id"]);
+    $eventTable->set("start_time", $data["start_time"]);
+    $eventTable->set("end_time", $data["end_time"]);
+    $eventTable->set("frequency", $data["frequency"]);
+    $eventTable->setAssociativeArray("times", []);
+    $eventTable->setAssociativeArray("entries", []);
+
+    try {
+        $eventTable->save();
+    } catch (ParseException $ex) {
+        $reply = array('ok' => false, 'error' => $ex->getMessage());
+        echo json_encode($reply);
+        return;
+    }
+
+    $reply = array('ok' => true, 'event_id'=>$data["event_id"]);
+    echo json_encode($reply);
 });
 
 $app->run();
