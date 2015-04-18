17151c59c1bd94063f8de570e857ea5cd8624efa
Oliver Tan <sinovercosequalstan@gmail.com>
2015-04-18 14:10:43 +1000

    login

---
+++ app/config.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use Facebook\FacebookSession;
+
+FacebookSession::setDefaultApplication('794867827234077', 'a400da5218753a7d50cf9e44e1b581f0');
+?>
--- app/index.php
+++ app/index.php
@@ -1,4 +1,44 @@
 <?php
-    require_once __DIR__ . '/vendor/autoload.php';
-    echo "asdf";
-?>
+require_once __DIR__ . '/vendor/autoload.php';
+require_once 'config.php';
+
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+
+// start session
+session_start();
+
+// login helper with redirect_uri
+$helper = new FacebookRedirectLoginHelper('http://fb.jcaw.me/fb/app');
+
+try {
+  $session = $helper->getSessionFromRedirect();
+} catch( FacebookRequestException $ex ) {
+  // When Facebook returns an error
+} catch( Exception $ex ) {
+  // When validation fails or other local issues
+}
+
+// see if we have a session
+if (isset($session)) {
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject();
+
+  // print data
+  echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
+} else {
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
+}
+
+$_SESSION["fb-auth"] = $session->getToken();
+?>i
